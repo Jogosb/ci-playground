0fce028ed279cc750684c4771f107905c492f3ca
Jogosb <filip.radon@gmail.com>
2019-07-25 10:42:20 +0200

    Added sample test to check exporting test outputs

---
+++ app/src/test/java/com/filipradon/ciplayground/SampleTest.kt
@@ -0,0 +1,12 @@
+package com.filipradon.ciplayground
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SampleTest {
+
+    @Test
+    fun `just a test`() {
+        assertEquals(true,  2 == 2)
+    }
+}
